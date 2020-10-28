802d30bc0540f670b69fe6a20845ebbf2595d074
Alexander Sommer <xancoder@gmail.com>
2020-10-28 12:14:34 +0100

    feat(collatz): add csharp version

---
+++ CSharp/Collatz/Collatz.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Collatz
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            const int inputNumber = 27;
+            Console.Out.WriteLine(collatz_iteration(inputNumber));
+            collatz_recursive(inputNumber);
+        }
+
+        private static int collatz_iteration(int number)
+        {
+            var steps = 0;
+            while (number != 1)
+            {
+                Console.Out.Write(number + " ");
+                if (number % 2 == 0)
+                {
+                    number /= 2;
+                }
+                else
+                {
+                    number = 3 * number + 1;
+                }
+
+                steps++;
+            }
+
+            Console.Out.WriteLine(number);
+            return steps;
+        }
+
+        private static void collatz_recursive(int number)
+        {
+            Console.Out.Write(number + " ");
+            if (number == 1)
+            {
+                return;
+            }
+
+            if (number % 2 == 0)
+            {
+                collatz_recursive(number / 2);
+            }
+            else
+            {
+                collatz_recursive(3 * number + 1);
+            }
+        }
+    }
+}
